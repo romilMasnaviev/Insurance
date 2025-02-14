1d1ef05fb0c049c6cfa5a1f05a016f4bbe5fa58c
romilMasnaviev <134401753+romilMasnaviev@users.noreply.github.com>
2025-02-14 22:13:00 +0300

    add Entities

---
+++ src/main/java/org/romilMasnaviev/travel/insurance/model/Classifier.java
@@ -0,0 +1,24 @@
+package org.romilMasnaviev.travel.insurance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "classifiers")
+public class Classifier {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+}
---
+++ src/main/java/org/romilMasnaviev/travel/insurance/model/ClassifierValue.java
@@ -0,0 +1,28 @@
+package org.romilMasnaviev.travel.insurance.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "classifier_values")
+public class ClassifierValue {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "classifier_id", referencedColumnName = "id", nullable = false)
+    private Classifier classifier;
+
+    @Column(name = "ic", nullable = false)
+    private String ic;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+}
